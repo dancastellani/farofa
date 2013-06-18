893a4f8bddcf0cd092351e406d9a1d72f869d555
Daniel Castellani <dancastellani@gmail.com>
2013-06-18 08:34:38 -0300

    Added command farofa

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,13 @@ function countrevisions{
 	echo "geting log..."
 	$svnlog = svn log -q --stop-on-copy $args;
 	echo "counting..."	
-	[regex]::matches($svnlog,"r\d+").count};
+	[regex]::matches($svnlog,"r\d+").count
+};
 Set-Alias cr countrevisions;
+
 function rm-rf {echo "rm -Recurse -Force $args";rm -Recurse -Force $args};
+
+function farofa {
+	echo "To see the list of commands and documentation check Farofa's website";
+	echo " - https://github.com/dancastellani/farofa"
+};
